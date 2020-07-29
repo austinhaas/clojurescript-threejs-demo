7520e45b622ac9a523d39ee145276312464e21ce
Austin Haas <austin@pettomato.com>
2020-07-28 17:32:52 -0700

    cdn-version: fixed

---
+++ cdn-version/externs.js
@@ -0,0 +1,18 @@
+var THREE;
+THREE.REVISION;
+THREE.Scene;
+THREE.PerspectiveCamera;
+THREE.WebGLRenderer;
+THREE.BoxGeometry;
+THREE.MeshBasicMaterial;
+THREE.Mesh;
+THREE.GridHelper;
+THREE.AxesHelper;
+THREE.DirectionalLight;
+THREE.TorusGeometry;
+THREE.MeshStandardMaterial;
+THREE.Mesh;
+THREE.PlaneGeometry;
+THREE.MeshStandardMaterial;
+THREE.Mesh;
+THREE.Fog;
--- cdn-version/src/demo.cljs
+++ cdn-version/src/demo.cljs
@@ -1,81 +1,82 @@
 (ns demo)
 
-(def THREE js/THREE)
-(def OrbitControls js/OrbitControls)
+(set! *warn-on-infer* true)
 
-(js/console.log (str "THREE.js " THREE/REVISION))
+(defn ^:export start []
 
-;;; https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
+  (def ^js THREE js/THREE)
+  (def ^js OrbitControls js/OrbitControls)
 
-(def scene (THREE.Scene.))
+  (js/console.log (str "THREE.js " THREE/REVISION))
 
-(def camera (THREE.PerspectiveCamera. 75
-                                      (/ (.-innerWidth  js/window)
-                                         (.-innerHeight js/window))
-                                      0.1
-                                      1000))
+  ;; https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
 
-(def renderer (THREE.WebGLRenderer.))
-(.setSize renderer
-          (.-innerWidth  js/window)
-          (.-innerHeight js/window))
-(.appendChild (.-body js/document) (.-domElement renderer))
+  (def ^js scene (THREE.Scene.))
 
-(def geometry (THREE.BoxGeometry.))
+  (def ^js camera (THREE.PerspectiveCamera. 75
+                                            (/ (.-innerWidth  js/window)
+                                               (.-innerHeight js/window))
+                                            0.1
+                                            1000))
 
-(def material (THREE.MeshBasicMaterial. (clj->js {:color 0x00ff00})))
+  (def ^js renderer (THREE.WebGLRenderer.))
+  (.setSize renderer
+            (.-innerWidth  js/window)
+            (.-innerHeight js/window))
+  (.appendChild (.-body js/document) (.-domElement renderer))
 
-(def cube (THREE.Mesh. geometry material))
-(.add scene cube)
+  (def ^js geometry (THREE.BoxGeometry.))
 
-(set! (.. camera -position -z) 5)
+  (def ^js material (THREE.MeshBasicMaterial. (clj->js {:color 0x00ff00})))
 
-(defn animate []
-  (js/requestAnimationFrame animate)
-  (set! (.. cube -rotation -x) (+ (.. cube -rotation -x) 0.01))
-  (set! (.. cube -rotation -y) (+ (.. cube -rotation -y) 0.01))
-  (.render renderer scene camera))
+  (def ^js cube (THREE.Mesh. geometry material))
+  (.add scene cube)
 
-(animate)
+  (set! (.. camera -position -z) 5)
 
-;;; Add something from the examples directory
+  (defn animate []
+    (js/requestAnimationFrame animate)
+    (set! (.. cube -rotation -x) (+ (.. cube -rotation -x) 0.01))
+    (set! (.. cube -rotation -y) (+ (.. cube -rotation -y) 0.01))
+    (.render renderer scene camera))
 
-(def controls (OrbitControls. camera (.-domElement renderer)))
-(.update controls)
+  (animate)
+
+  ;;; Add something from the examples directory
+  (def ^js controls (OrbitControls. camera (.-domElement renderer)))
+  (.update controls))
 
 (defn add-scene!
   "Adds more things to the global scene. You can call this function from the REPL,
-  or, if you can evaluate code from your editor, you can try evaluating each
-  expression in order."
+    or, if you can evaluate code from your editor, you can try evaluating each
+    expression in order."
   []
 
   (.set (.-position camera) 5 5 5)
   (.lookAt camera 0 0 0)
 
-  (def grid-helper (THREE.GridHelper. 10 10))
+  (def ^js grid-helper (THREE.GridHelper. 10 10))
   (.add scene grid-helper)
 
-  (def axes-helper (THREE.AxesHelper. 5))
+  (def ^js axes-helper (THREE.AxesHelper. 5))
   (.add scene axes-helper)
 
-  (def light (THREE.DirectionalLight. 0xffffff 1))
+  (def ^js light (THREE.DirectionalLight. 0xffffff 1))
   (.add scene light)
 
-  (def torus-geometry (THREE.TorusGeometry. 1 0.4 80 60))
-  (def torus-material (THREE.MeshStandardMaterial. (clj->js {:color 0x3333ff})))
-  (def torus (THREE.Mesh. torus-geometry torus-material))
+  (def ^js torus-geometry (THREE.TorusGeometry. 1 0.4 80 60))
+  (def ^js torus-material (THREE.MeshStandardMaterial. (clj->js {:color 0x3333ff})))
+  (def ^js torus (THREE.Mesh. torus-geometry torus-material))
   (.add scene torus)
   (.set (.-position torus) -2 1 -1)
   (.set (.-rotation torus) -0.8 0.6 0)
 
-  (def plane-geometry (THREE.PlaneGeometry. 8 8))
-  (def plane-material (THREE.MeshStandardMaterial. (clj->js {:color 0xff3333})))
-  (def plane (THREE.Mesh. plane-geometry plane-material))
+  (def ^js plane-geometry (THREE.PlaneGeometry. 8 8))
+  (def ^js plane-material (THREE.MeshStandardMaterial. (clj->js {:color 0xff3333})))
+  (def ^js plane (THREE.Mesh. plane-geometry plane-material))
   (.add scene plane)
   (.set (.-position plane) -1 1 -2)
   (.set (.-rotation plane) -0.8 0.5 0)
 
-  (def fog (THREE.Fog. 0x333333 1 30))
-  (set! (.-fog scene) fog)
-
-  )
+  (def ^js fog (THREE.Fog. 0x333333 1 30))
+  (set! (.-fog scene) fog))
